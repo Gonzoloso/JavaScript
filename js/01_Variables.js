8fac0786468796211c016e6267e55e559693b4b6
Gonzoloso <ivanovichmaga@gmail.com>
2021-08-25 14:19:30 -0500

    00_holamundo & 01_Variables.js aad dead

---
+++ js/01_Variables.js
@@ -0,0 +1,13 @@
+document.write(" <h1>Variables</h1>");
+var pais = "Mexico";
+var continente = "Americano";
+var todojunto = pais+continente;
+
+var obj = {};
+var arre = [1,2,4];
+
+console.log(todojunto);
+// Concatenar , o +
+console.log("El pais",pais,"esta ubicado en el continente",  continente);
+
+console.log(arre);
